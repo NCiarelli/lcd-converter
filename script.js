1a00c927bd05ebb5ba88a31e497dc06cff153708
Ncia89 <nicholas.ciarelli@gmail.com>
2019-10-24 16:39:42 -0400

    Finished creating and testing the builder functions for the rest of the numerals.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // console.log("Hello");
 // Your task is to create an LCD string representation of an integer value using a 3x3 grid of space, underscore, and pipe characters for each digit.
 
-let lcdNumberFunctions = {};
+let lcdNumberFunctions = [];
 // Strings Builder Function for Numeral 0
 let makeStringsForZero = (linesToDisplay) => {
   linesToDisplay[0] += " _  ";
@@ -22,25 +22,81 @@ let makeStringsForOne = (linesToDisplay) => {
 lcdNumberFunctions.push(makeStringsForOne);
 
 // Strings Builder Function for Numeral 2
+let makeStringsForTwo = (linesToDisplay) => {
+  linesToDisplay[0] += " _  ";
+  linesToDisplay[1] += " _| ";
+  linesToDisplay[2] += "|_  ";
+  return linesToDisplay;
+}
+lcdNumberFunctions.push(makeStringsForTwo);
 
 // Strings Builder Function for Numeral 3
+let makeStringsForThree = (linesToDisplay) => {
+  linesToDisplay[0] += " _  ";
+  linesToDisplay[1] += " _| ";
+  linesToDisplay[2] += " _| ";
+  return linesToDisplay;
+}
+lcdNumberFunctions.push(makeStringsForThree);
 
 // Strings Builder Function for Numeral 4
+let makeStringsForFour = (linesToDisplay) => {
+  linesToDisplay[0] += "    ";
+  linesToDisplay[1] += "|_| ";
+  linesToDisplay[2] += "  | ";
+  return linesToDisplay;
+}
+lcdNumberFunctions.push(makeStringsForFour);
 
 // Strings Builder Function for Numeral 5
+let makeStringsForFive = (linesToDisplay) => {
+  linesToDisplay[0] += " _  ";
+  linesToDisplay[1] += "|_  ";
+  linesToDisplay[2] += " _| ";
+  return linesToDisplay;
+}
+lcdNumberFunctions.push(makeStringsForFive);
 
 // Strings Builder Function for Numeral 6
+let makeStringsForSix = (linesToDisplay) => {
+  linesToDisplay[0] += " _  ";
+  linesToDisplay[1] += "|_  ";
+  linesToDisplay[2] += "|_| ";
+  return linesToDisplay;
+}
+lcdNumberFunctions.push(makeStringsForSix);
 
 // Strings Builder Function for Numeral 7
+let makeStringsForSeven = (linesToDisplay) => {
+  linesToDisplay[0] += " _  ";
+  linesToDisplay[1] += "  | ";
+  linesToDisplay[2] += "  | ";
+  return linesToDisplay;
+}
+lcdNumberFunctions.push(makeStringsForSeven);
 
 // Strings Builder Function for Numeral 8
+let makeStringsForEight = (linesToDisplay) => {
+  linesToDisplay[0] += " _  ";
+  linesToDisplay[1] += "|_| ";
+  linesToDisplay[2] += "|_| ";
+  return linesToDisplay;
+}
+lcdNumberFunctions.push(makeStringsForEight);
 
 // Strings Builder Function for Numeral 9
+let makeStringsForNine = (linesToDisplay) => {
+  linesToDisplay[0] += " _  ";
+  linesToDisplay[1] += "|_| ";
+  linesToDisplay[2] += "  | ";
+  return linesToDisplay;
+}
+lcdNumberFunctions.push(makeStringsForNine);
 
 
 function lcd(numToDisplay) {
   let lines = ["", "", ""];
-  numString = numToDisplay.toString();
+  let numString = numToDisplay.toString();
   // Go through each digit of the input number
   for (let i = 0; i < numString.length; i++) {
     // Add each numeral representation to the lines strings using the functions in the lcdNumberFunctions array
@@ -52,3 +108,4 @@ function lcd(numToDisplay) {
   }
 }
 
+lcd(1234567890);
